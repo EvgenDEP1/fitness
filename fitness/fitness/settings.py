7a5351c2305ca91a6feaede00c80002ee80a476e
EvgenDEP1 <jen_dostovalov@mail.ru>
2021-10-19 14:38:38 +0500

    2021

--- fitness/fitness/settings.py
+++ fitness/fitness/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-et=wr7+k#m=2hh+vxbu)k_-ihhl^e^0gsfpusw197(7#7225gc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3th part apps
     'rest_framework',
-    # 'corsheaders',
+    'corsheaders',
     # own apps
     'authapp',
     'mainapp',
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +127,10 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authapp.UserProfile'
+
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    # "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
--- fitness/front/src/App.js
+++ fitness/front/src/App.js
@@ -14,278 +14,8 @@ import TrainerList from "./components/TrainerList";
 import axios from "axios";
 
 
-const usersMock = [
-    {
-    "id": 1,
-    "username": "user1",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 2,
-    "username": "user2",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 3,
-    "username": "user3",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 4,
-    "username": "user4",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 5,
-    "username": "user5",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 6,
-    "username": "user6",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 7,
-    "username": "user7",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 8,
-    "username": "user8",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 9,
-    "username": "user9",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 10,
-    "username": "user10",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  },
-  {
-    "id": 11,
-    "username": "kpk",
-    "email": "",
-    "first_name": "",
-    "last_name": "",
-    "gender": "",
-    "date_birth": null
-  }
-];
-
-const servicesMock = [
-    {
-    "id": 1,
-    "name": "Услуга 1",
-    "price": "200.00",
-    "desc": "тест",
-    "status": false
-  },
-  {
-    "id": 2,
-    "name": "Услуга 2",
-    "price": "200.00",
-    "desc": "тест",
-    "status": false
-  },
-  {
-    "id": 3,
-    "name": "Услуга 3",
-    "price": "200.00",
-    "desc": "тест",
-    "status": false
-  },
-  {
-    "id": 4,
-    "name": "Услуга 4",
-    "price": "200.00",
-    "desc": "тест",
-    "status": false
-  },
-  {
-    "id": 5,
-    "name": "Услуга 5",
-    "price": "200.00",
-    "desc": "тест",
-    "status": false
-  }
-];
-
-const recordingsMock = [
-    {
-    "id": 1,
-    "created": "2021-10-11T05:33:54.795389Z",
-    "updated": "2021-10-11T05:33:54.863361Z",
-    "service": 1,
-    "trainer": [
-      1
-    ],
-    "user": [
-      1,
-      2,
-      3
-    ]
-  },
-  {
-    "id": 2,
-    "created": "2021-10-11T05:33:54.795389Z",
-    "updated": "2021-10-11T05:33:54.863361Z",
-    "service": 2,
-    "trainer": [
-      2,
-      3,
-      4
-    ],
-    "user": [
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10
-    ]
-  },
-  {
-    "id": 3,
-    "created": "2021-10-11T05:33:54.795389Z",
-    "updated": "2021-10-11T05:33:54.863361Z",
-    "service": 3,
-    "trainer": [
-      5
-    ],
-    "user": [
-      6
-    ]
-  },
-  {
-    "id": 4,
-    "created": "2021-10-11T05:33:54.795389Z",
-    "updated": "2021-10-11T05:33:54.863361Z",
-    "service": 4,
-    "trainer": [
-      2
-    ],
-    "user": [
-      3,
-      7
-    ]
-  },
-  {
-    "id": 5,
-    "created": "2021-10-11T05:33:54.795389Z",
-    "updated": "2021-10-11T05:33:54.863361Z",
-    "service": 5,
-    "trainer": [
-      4
-    ],
-    "user": [
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8
-    ]
-  }
-];
-
-const trainersMock = [
-    {
-    "id": 1,
-    "name": "Имя 1",
-    "surname": "Фамилия 1",
-    "patronymic": "Отчество 1",
-    "gender": "",
-    "address": "адрес",
-    "phone_number": ""
-  },
-  {
-    "id": 2,
-    "name": "Имя 2",
-    "surname": "Фамилия 2",
-    "patronymic": "Отчество 2",
-    "gender": "",
-    "address": "адрес",
-    "phone_number": ""
-  },
-  {
-    "id": 3,
-    "name": "Имя 3",
-    "surname": "Фамилия 3",
-    "patronymic": "Отчество 3",
-    "gender": "",
-    "address": "адрес",
-    "phone_number": ""
-  },
-  {
-    "id": 4,
-    "name": "Имя 4",
-    "surname": "Фамилия 4",
-    "patronymic": "Отчество 4",
-    "gender": "",
-    "address": "адрес",
-    "phone_number": ""
-  },
-  {
-    "id": 5,
-    "name": "Имя 5",
-    "surname": "Фамилия 5",
-    "patronymic": "Отчество 5",
-    "gender": "",
-    "address": "адрес",
-    "phone_number": "9046587416"
-  },
-];
-
+const API_URL = "http://localhost:8000"
+const getURL = (suffix) => `${API_URL}/api/${suffix}/`
 
 
 class App extends React.Component {
@@ -301,18 +31,55 @@ class App extends React.Component {
 
     componentDidMount() {
         axios
-            .get("http://localhost:8000/api/users/")
+            .get(getURL("users"))
             .then((result) => {
-                console.log('result', result)
+                // console.log('users result', result)
+                this.setState({
+                users: result.data,
+
+                })
             })
             .catch((error) => console.log(error));
 
-        this.setState({
-                users: usersMock,
-                services: servicesMock,
-                recordings: recordingsMock,
-                trainers: trainersMock
+        axios
+            .get(getURL("services"))
+            .then((result) => {
+                // console.log('services result', result)
+                this.setState({
+                services: result.data,
+
+                })
             })
+            .catch((error) => console.log(error));
+
+        axios
+            .get(getURL("recordings"))
+            .then((result) => {
+                // console.log('services result', result)
+                this.setState({
+                recordings: result.data,
+
+                })
+            })
+            .catch((error) => console.log(error));
+
+        axios
+            .get(getURL("trainers"))
+            .then((result) => {
+                // console.log('services result', result)
+                this.setState({
+                trainers: result.data,
+
+                })
+            })
+            .catch((error) => console.log(error));
+
+        // this.setState({
+                // users: usersMock,
+                // services: servicesMock,
+                // recordings: recordingsMock,
+                // trainers: trainersMock
+            // })
     }
 
     render() {
